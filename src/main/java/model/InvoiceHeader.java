a9bdf422eecef30037e15657e6950d21c5ab0b7c
Mahmoud Mustafa <mahmoudmustafa038@gmail.com>
2022-07-20 05:01:47 +0200

    Project Construction Commit

---
+++ src/main/java/model/InvoiceHeader.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class InvoiceHeader {
+    
+    int invoiceNum;
+    Date invoiceDate;
+    String customerName;
+    ArrayList<InvoiceLine> lines;
+    
+    
+}
